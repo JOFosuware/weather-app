c4312b4cc8a2606e361bb09337e2480e526369a7
JOFosuware <ofosuwarejustice@gmail.com>
2020-05-15 13:56:33 +0000

    Init commit

---
+++ src/app.js
@@ -0,0 +1,87 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const forecast = require('./utils/forecast')
+
+const app = express()
+
+//Setting up views path
+const publicPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../template/views')
+const partialsPath = path.join(__dirname, '../template/partials')
+
+//Setting up handlebars and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setting up static location
+app.use(express.static(publicPath))
+
+app.get('',(req, res) => {
+    res.render('index',{
+        title: "Weather App",
+        name: "Jofosuware"
+    })
+})
+
+app.get('/about',(req, res) => {
+    res.render('about',{
+        title: "Weather Robot",
+        name: "Jofosuware"
+    })
+})
+
+app.get('/help',(req, res) => {
+    res.render('help', {
+        title: "Weather Help",
+        message: "Do you need any help, pour your questions here.",
+        name: "Jofosuware"
+    })
+})
+app.get('/weather', (req, res) => {
+    if(!req.query.address){
+        return res.send({
+            error: "You must provide an address"
+        })
+    }
+    forecast(req.query.address, (error, data) => {
+
+        if(error){
+            res.send({
+                error: "Enter correct address"
+            })
+        }else{
+            res.send({
+                Temperature: data,
+                Location: req.query.address
+    
+            })
+        }
+        
+        }
+    )
+})
+
+
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: "404",
+        _404Msg: "Help article not found",
+        name: "Jofosuware"
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: "404",
+        _404Msg: "Page not found",
+        name: "Jofosuware"
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (address, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=90d31cebab42ade037f7590110d0ac2a&query=" + address
+    request({url, json: true}, (error, {body}) => {
+        if(error){
+            callback('Unable to connect weather services', undefined)
+        }else if(body.error)
+            callback('Unable to fetch weather conditions')
+        else{
+            callback(undefined,  body.current.temperature)
+        }
+    })
+}
+
+
+module.exports = forecast
